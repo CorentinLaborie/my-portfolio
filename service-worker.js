472c12e940e5ee90729fa3d63899b0363c9b0810
Corentin Laborie <corentin.laborie@epitech.eu>
2020-08-28 00:08:50 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.c0cd9744d7dd3869b6ba0476e7a1d125.js"
+  "/my-portfolio/precache-manifest.849c569b1004c5b0270c238879cb8834.js"
 );
 
 self.addEventListener('message', (event) => {
